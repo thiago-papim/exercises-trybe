d34182e0665ddc7030415024e605c321a3c51c29
thiago-papim <thiago.fourp@gmail.com>
2022-11-03 18:37:02 -0300

    Atualizando

--- 01-fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicio3.js
+++ 01-fundamentos/sessao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicio3.js
@@ -47,12 +47,54 @@ leftContent.appendChild(imagem1);
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 
 const listaNaoOrdenada = document.createElement("ul");
+listaNaoOrdenada.className = "lista";
 rigthContent.appendChild(listaNaoOrdenada);
+
 let arrayLista = ["um", "dois", "tres", 'quatro', "cinco", "seis", "sete", "oito", "nove", "dez"];
 for (let index = 0; index < arrayLista.length; index += 1){
-  listaNaoOrdenada.createElement("li")
+  let lista = document.createElement("li")
+  lista.innerHTML = arrayLista[index];
+  listaNaoOrdenada.appendChild(lista)
 }
+
 
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 
-console.log(body);
+for (let index = 0; index <= 2; index += 1){
+  let h3 = document.createElement("h3");
+  main.appendChild(h3);
+}
+
+// Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
+
+// Adicione a classe title na tag h1 criada;
+
+textoH1.className = "title"
+
+// Adicione a classe description nas 3 tags h3 criadas;
+
+let classH3 = document.querySelectorAll("h3");
+for (let index = 0; index < classH3.length; index += 1){
+  classH3[index].className = "oi"
+}
+
+// Remova a section criado no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild();
+
+main.removeChild(leftContent);
+
+// Centralize a section criado no passo 6 (aquele que possui a classe right-content). Dica: para centralizar, basta configurar o margin-right: auto da section;
+
+rigthContent.style.marginRight = "auto";
+
+// Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
+
+centerContent.style.backgroundColor = "green"
+
+// Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
+
+const lista = rigthContent.firstElementChild;
+const ultimo = rigthContent.firstElementChild.lastElementChild;
+const penultimo = rigthContent.firstElementChild.lastElementChild.previousElementSibling;
+
+penultimo.remove(lista);
+ultimo.remove(lista);
